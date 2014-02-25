ccfee5519a0c93f3b52566f7374f79c7213ee204
Xin Wang <wangamanda.lu@gmail.com>
2014-02-24 20:21:51 -0500

    end of chap7

--- chap7/KthPrimeMultiple.java
+++ chap7/KthPrimeMultiple.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class KthPrimeMultiple{
 	public static void main(String args[]) throws Exception{
-		int k = 10;
+		int k = 15;
 		int r = KthMultiple(k);
 		System.out.println(r + "");
 	}
@@ -18,37 +18,11 @@ public class KthPrimeMultiple{
 			throw new Exception("Element k " + k + " is illegal!");
 		}
 		
-		if(k >= 1){
-			val = 3;
-			q3.add(val);
-			if(k > 1){
-				q3.add(val*3);
-				q3.remove();
-				q5.add(val*5);
-				q7.add(val*7);
-			}
-		}
-		
-		if(k >= 2){
-			val = 5;
-			q5.add(val);
-			if(k > 2){
-				q5.add(val*5);
-				q5.remove();
-				q7.add(val*7);
-			}
-		}
-		
-		if(k >= 3){
-			val = 7;
-			q7.add(val);
-			if(k > 3){
-				q7.add(val*7);
-				q7.remove();
-			}
-		}
+		q3.add(3);
+		q5.add(5);
+		q7.add(7);
 
-		for (int i = 4 ; i <= k ; i ++ ){
+		for (int i = 1 ; i <= k ; i ++ ){
 			val = Math.min(q3.peek(), Math.min(q5.peek(), q7.peek()));
 			if(val == q3.peek()){
 				q3.add(val*3);
