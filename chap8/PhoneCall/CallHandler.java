c811c612f8ef632550e6af31e4727508f9cc1e7e
Xin Wang <wangamanda.lu@gmail.com>
2014-02-28 23:18:34 -0500

    CallHandler

--- chap8/PhoneCall/CallHandler.java
+++ chap8/PhoneCall/CallHandler.java
@@ -30,5 +30,46 @@ public class CallHandler{
 			directors.add(new Director());
 		}
 		employeeLevels.add(directors);
+
+		callQueues = new ArrayList<ArrayList<Call>>(LEVELS);
+	}
+
+	public Employee getHandlerForCall(Call call){
+		int rank = call.getRank().getValue();
+		for (int i = rank ; i < LEVELS ; i ++ ){
+			ArrayList<Employee> employees = employeeLevels.get(i);
+			for (int j = 0 ; j < employees.size() ; j ++ ){
+				employee = employees.get(j);
+				if (employee != null){
+					return employee;
+				}
+			}
+		}
+		return null;
+	}
+
+	public void dispatchCall(Caller caller){
+		Call call = new Call(caller);
+		Employee employee = getHandlerForCall(call);
+		if(employee != null){
+			call.setHandler(employee);
+			employee.receiveCall(call);
+		}else{
+			call.reply("Please wait a second.");
+			callQueues.get(call.getRank().getValue()).add(call);
+		}
+	}
+
+	public void assignCall(Employee employee){
+		int rank = callQueues.getRank().getValue();
+		for (int i = rank ; i >= 0 ; i --){
+			ArrayList<call> callQue = callQueues.get(i);
+			if (callQue.size() > 0){
+				Call call = callQue.remove(0);
+				call.setHandler(employee);
+				return;
+			}
+		}
+		employee.setFree();
 	}
 }
