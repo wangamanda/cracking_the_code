71c4ec52b37911535868340ce4667208ab43a9b3
Xin Wang <wangamanda.lu@gmail.com>
2014-03-01 16:55:13 -0500

    call

---
+++ chap8/PhoneCall/Call.java
@@ -0,0 +1,41 @@
+package PhoneCall;
+
+public class Call{
+	private Title rank;
+	private Caller caller;
+	private Handler employee;
+
+	public Call(Caller c){
+		caller = c;
+		rank = Title.Respondent;
+	}
+
+	public void setRank(Title r){
+		rank = r;
+	}
+
+	public Title getRank(){
+		return rank;
+	}
+
+	public void reply(String s){
+		System.out.println(s);
+	}
+
+	public void disconnect(){
+		System.out.println("Thank you for calling.");
+	}
+
+	public void setHandler(Employee emp){
+		employee = emp;
+	}
+
+	public Title incrementRank(){
+		if (rank == Title.Respondent){
+			setRank(Title.Manager);
+		}else if (rank == Title.Manager){
+			setRank(Title.Director);
+		}
+		return rank;
+	}
+}
