c3e59ca818f33872b119e7b5392716e8bc6b6579
Xin Wang <wangamanda.lu@gmail.com>
2014-03-03 04:38:20 -0500

    book&user

---
+++ chap8/BookReader/Book.java
@@ -0,0 +1,42 @@
+package BookReader;
+
+public class Book{
+	private String name;
+	private String author;
+	private int id;
+	private User reader;
+
+	public Book(String n, String a, int i){
+		name = n;
+		author = a;
+		id = i;
+	}
+
+	public int getID(){
+		return id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getAuthor(){
+		return Author;
+	}
+
+	public void setID(int i){
+		id = i;
+	}
+
+	public User getReader(){
+		return reader;
+	}
+
+	public void setReader(User r){
+		reader = r;
+	}
+
+	public boolean isAvailable(){
+		return reader == null;
+	}
+}
---
+++ chap8/BookReader/User.java
@@ -0,0 +1,52 @@
+package BookReader;
+
+import java.util.Set;
+
+public class User{
+	private int uid;
+	private String uname;
+	private Set<Book> books;
+
+	public User(int i, String n){
+		uid = i;
+		uname = n;
+		books = new Set<Book>();
+	}
+
+	public int getID(){
+		return uid;
+	}
+
+	public void setID(int i){
+		uid = i;
+	}
+
+	public String getReaderName(){
+		return uname;
+	}
+
+	public String setReaderName(String n){
+		uname = n;
+	}
+
+	public boolean borrowBook(Book b){
+		if(b.isAvailable()){
+			books.add(b);
+			b.setReader(this);
+			return true;
+		}
+		return false;
+	}
+
+	public void renewMembership(){
+	
+	}
+
+	public boolean returnBook(Book b){
+		if(books.contains(b)){
+			b.setReader(null);
+			return books.remove(b);
+		}
+		return false;
+	}
+}
