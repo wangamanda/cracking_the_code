13c1ed72bc4deca4e521721c9c53edb570a8f249
Xin Wang <wangamanda.lu@gmail.com>
2014-03-12 21:45:47 -0400

    trailingzero

---
+++ chap17/TrailingZero.java
@@ -0,0 +1,15 @@
+public class TrailingZero{
+	public static void main(String[] args){
+		int n = Integer.parseInt(args[0]);
+		int res = Zeros(n);
+		System.out.println(res+"");
+	}
+
+	public static int Zeros(int n){
+		int k = 1;
+		for (int i = 5 ; i < n ; i += 5){
+			k ++ ;
+		}
+		return k;
+	}
+}
