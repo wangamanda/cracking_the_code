d9afc2749e0c90e84a2a0dd211c8ba2665a4d601
Xin Wang <wangamanda.lu@gmail.com>
2014-03-13 23:54:53 -0400

    maximum

--- chap17/Maximum.java
+++ chap17/Maximum.java
@@ -1,7 +1,7 @@
 public class Maximum{
 	public static void main(String[] args){
-		int a = args[0];
-		int b = args[1];
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
 		int c = a-b;
 		int sign_a = sign(a);
 		int sign_b = sign(b);
