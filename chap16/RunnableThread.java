3a5349eb3d26311cf54a15ddc76e5f091df9230e
Xin Wang <wangamanda.lu@gmail.com>
2014-03-19 22:53:08 -0400

    runnable

---
+++ chap16/RunnableThread.java
@@ -0,0 +1,29 @@
+import java.lang.Runnable;
+
+public class RunnableThread implements Runnable{
+	static int count = 0;
+	public void run(){
+		try{
+			while(count < 5){
+				Thread.sleep(500);
+				count++;
+			}
+		}catch(InterruptedException exp){
+			System.out.println("interrupted exception.");
+		}
+	}
+
+	public static void main(String[] args){
+		RunnableThread instance = new RunnableThread();
+		Thread thread = new Thread(instance);
+		thread.start();
+		try{
+			while(count < 5){
+				thread.sleep(250);
+				count ++ ;
+			}
+		}catch(InterruptedException exp){
+			System.out.println("interrupted exception.");
+		}
+	}
+}
