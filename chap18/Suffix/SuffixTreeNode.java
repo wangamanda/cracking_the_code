8a649d1d5df269be6b784e8f0149cc4a6df2bc7e
Xin Wang <wangamanda.lu@gmail.com>
2014-04-02 20:21:46 -0400

    suffix

--- chap18/Suffix/SuffixTreeNode.java
+++ chap18/Suffix/SuffixTreeNode.java
@@ -1,7 +1,30 @@
 package Suffix;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class SuffixTreeNode{
+	char value;
+	HashMap<Character, SuffixTreeNode> children = new HashMap<Character, SuffixTreeNode>();
+	ArrayList<Integer> arr = new ArrayList<Integer>();
 
+	public SuffixTreeNode(){
+	
+	}
+
+	public void insert(String str, int index){
+		arr.add(index);
+		char first = str.charAt(0);
+		value = first;
+		String sub = String.substring(1);
+		SuffixTreeNode child = null;
+		if(children.containsKey(first)){
+			child = children.get(first);
+		}else{
+			child = new SuffixTreeNode();
+			children.put(first, child);
+		}
+		
+		child.insert(sub, index);
+	}
 }
