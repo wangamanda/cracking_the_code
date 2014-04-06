c5e30e20e03e4edf2406a7e519ae0c2ba54929dc
Xin Wang <wangamanda.lu@gmail.com>
2014-04-06 12:55:15 -0400

    semaphore

--- chap16/Semaphores/Foo.java
+++ chap16/Semaphores/Foo.java
@@ -6,16 +6,58 @@ import java.util.concurrent.Semaphore;
 
 public class Foo{
 
+	public Semaphore sem1;
+	public Semaphore sem2;
+	public Semaphore sem3;
+	public int pauseTime = 1000;
+
 	public Foo(){
 		try{
-			Semaphore sem1 = new Semaphore(1);
-			Semaphore sem2 = new Semaphore(1);
-			Semaphore sem3 = new Semaphore(1);
-			sem1.aquire();
-			sem2.aquire();
-			sem3.aquire();
+			sem1 = new Semaphore(1);
+			sem2 = new Semaphore(1);
+			sem3 = new Semaphore(1);
+			sem1.acquire();
+			sem2.acquire();
+			sem3.acquire();
 		}catch(Exception ex){
-			return ex.printStackTrace();
+			ex.printStackTrace();
+		}
+	}
+
+	public void First(){
+		try{
+			System.out.println("First starts");
+			Thread.sleep(pauseTime);
+			System.out.println("First ends");
+			sem1.release();
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+	public void Second(){
+		try{
+			sem1.acquire();
+			sem1.release();
+			System.out.println("Second starts");
+			Thread.sleep(pauseTime);
+			System.out.println("Second ends");
+			sem2.release();
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+	public void Third(){
+		try{
+			sem2.acquire();
+			sem2.release();
+			System.out.println("Third starts");
+			Thread.sleep(pauseTime);
+			System.out.println("Third ends");
+			sem3.release();
+		}catch(Exception ex){
+			ex.printStackTrace();
 		}
 	}
 }
